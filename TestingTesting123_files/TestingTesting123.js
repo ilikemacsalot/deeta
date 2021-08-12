6dd948502f0d92092994a022432478474698507f
ilikemacsalot <81637330+ilikemacsalot@users.noreply.github.com>
2021-08-12 10:37:18 -0500

    site?
    w

---
+++ TestingTesting123_files/TestingTesting123.js
@@ -0,0 +1,30 @@
+// Created by iWeb 3.0.2 local-build-20210812
+
+function writeMovie1()
+{detectBrowser();if(windowsInternetExplorer)
+{document.write('<object id="id2" classid="clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B" codebase="http://www.apple.com/qtactivex/qtplugin.cab" width="232" height="128" style="height: 128px; left: 319px; position: absolute; top: 2px; width: 232px; z-index: 1; "><param name="src" value="Media/AWESOME.EDE-1.mov" /><param name="controller" value="true" /><param name="autoplay" value="false" /><param name="scale" value="tofit" /><param name="volume" value="100" /><param name="loop" value="false" /></object>');}
+else if(isiPhone)
+{document.write('<object id="id2" type="video/quicktime" width="232" height="128" style="height: 128px; left: 319px; position: absolute; top: 2px; width: 232px; z-index: 1; "><param name="src" value="TestingTesting123_files/AWESOME.EDE.jpg"/><param name="target" value="myself"/><param name="href" value="../Media/AWESOME.EDE-1.mov"/><param name="controller" value="true"/><param name="scale" value="tofit"/></object>');}
+else
+{document.write('<object id="id2" type="video/quicktime" width="232" height="128" data="Media/AWESOME.EDE-1.mov" style="height: 128px; left: 319px; position: absolute; top: 2px; width: 232px; z-index: 1; "><param name="src" value="Media/AWESOME.EDE-1.mov"/><param name="controller" value="true"/><param name="autoplay" value="false"/><param name="scale" value="tofit"/><param name="volume" value="100"/><param name="loop" value="false"/></object>');}}
+function writeMovie2()
+{detectBrowser();if(windowsInternetExplorer)
+{document.write('<object id="id4" classid="clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B" codebase="http://www.apple.com/qtactivex/qtplugin.cab" width="246" height="201" style="height: 201px; left: 30px; position: absolute; top: 0px; width: 246px; z-index: 1; "><param name="src" value="Media/Kitchen%20Gun%20-%20In%20stores%20now%21%20-%20BBC-1.mp4" /><param name="controller" value="true" /><param name="autoplay" value="false" /><param name="scale" value="tofit" /><param name="volume" value="100" /><param name="loop" value="false" /></object>');}
+else if(isiPhone)
+{document.write('<object id="id4" type="video/quicktime" width="246" height="201" style="height: 201px; left: 30px; position: absolute; top: 0px; width: 246px; z-index: 1; "><param name="src" value="TestingTesting123_files/Kitchen%20Gun%20-%20In%20stores%20now%21%20-%20BBC.jpg"/><param name="target" value="myself"/><param name="href" value="../Media/Kitchen%20Gun%20-%20In%20stores%20now%21%20-%20BBC-1.mp4"/><param name="controller" value="true"/><param name="scale" value="tofit"/></object>');}
+else
+{document.write('<object id="id4" type="video/quicktime" width="246" height="201" data="Media/Kitchen%20Gun%20-%20In%20stores%20now%21%20-%20BBC-1.mp4" style="height: 201px; left: 30px; position: absolute; top: 0px; width: 246px; z-index: 1; "><param name="src" value="Media/Kitchen%20Gun%20-%20In%20stores%20now%21%20-%20BBC-1.mp4"/><param name="controller" value="true"/><param name="autoplay" value="false"/><param name="scale" value="tofit"/><param name="volume" value="100"/><param name="loop" value="false"/></object>');}}
+function writeMovie3()
+{detectBrowser();if(windowsInternetExplorer)
+{document.write('<object id="id5" classid="clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B" codebase="http://www.apple.com/qtactivex/qtplugin.cab" width="75" height="44" style="height: 44px; left: 617px; position: absolute; top: 32px; width: 75px; z-index: 1; "><param name="src" value="Media/i-1.mov" /><param name="controller" value="true" /><param name="autoplay" value="false" /><param name="scale" value="tofit" /><param name="volume" value="100" /><param name="loop" value="false" /></object>');}
+else if(isiPhone)
+{document.write('<object id="id5" type="video/quicktime" width="75" height="44" style="height: 44px; left: 617px; position: absolute; top: 32px; width: 75px; z-index: 1; "><param name="src" value="TestingTesting123_files/i.jpg"/><param name="target" value="myself"/><param name="href" value="../Media/i-1.mov"/><param name="controller" value="true"/><param name="scale" value="tofit"/></object>');}
+else
+{document.write('<object id="id5" type="video/quicktime" width="75" height="44" data="Media/i-1.mov" style="height: 44px; left: 617px; position: absolute; top: 32px; width: 75px; z-index: 1; "><param name="src" value="Media/i-1.mov"/><param name="controller" value="true"/><param name="autoplay" value="false"/><param name="scale" value="tofit"/><param name="volume" value="100"/><param name="loop" value="false"/></object>');}}
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('TestingTesting123_files/TestingTesting123Moz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');fixAllIEPNGs('Media/transparent.gif');Widget.onload();fixupAllIEPNGBGs();performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
